7c7fb3686872facfb74b8ac1495d1867a51bf18d
verion <bijacik.adam@gmail.com>
2023-10-25 15:01:38 +0200

    create spring app

---
+++ src/main/java/com/minder/MoneyMinder/MoneyMinderApplication.java
@@ -0,0 +1,13 @@
+package com.minder.MoneyMinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMinderApplication.class, args);
+	}
+
+}
