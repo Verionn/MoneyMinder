85e2594e57608686207a308ad8f69388d3efcb06
verion <bijacik.adam@gmail.com>
2023-11-11 11:58:35 +0100

    add more tests, rework update list method when given bad data

--- src/test/java/com/minder/MoneyMinder/list/UpdateListTests.java
+++ src/test/java/com/minder/MoneyMinder/list/UpdateListTests.java
@@ -1,19 +1,17 @@
 package com.minder.MoneyMinder.list;
 
+import com.jayway.jsonpath.internal.function.sequence.First;
 import com.minder.MoneyMinder.MoneyMinderApplicationTests;
 import com.minder.MoneyMinder.list.dto.ListResponse;
-import com.minder.MoneyMinder.list.dto.ListsResponse;
 import com.minder.MoneyMinder.list.dto.UpdateListRequestBody;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.http.HttpMethod.PUT;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 public class UpdateListTests extends MoneyMinderApplicationTests {
 
@@ -33,5 +31,53 @@ public class UpdateListTests extends MoneyMinderApplicationTests {
         assertThat(updateListResponse.getBody().name(), equalTo(NEW_LIST_NAME));
         assertThat(updateListResponse.getBody().fullPrice(), equalTo(NEW_FULL_PRICE));
         assertThat(updateListResponse.getStatusCode(), equalTo(OK));
+    }
+
+    @Test
+    @DisplayName("Should not update list and return not found when given wrong listId")
+    public void shouldNotUpdateListAndReturnNotFound(){
+        //given
+        var listId = WRONG_LIST_ID;
+        var updateListRequestBody = new UpdateListRequestBody(NEW_LIST_NAME, NEW_FULL_PRICE);
+
+        HttpEntity<UpdateListRequestBody> requestEntity = new HttpEntity<>(updateListRequestBody);
+
+        //when
+        var updateListResponse = client.exchange(listPath(listId), PUT, requestEntity, ListResponse.class);
+
+        //then
+        assertThat(updateListResponse.getStatusCode(), equalTo(NOT_FOUND));
+    }
+
+    @Test
+    @DisplayName("Should not update list and return 400 when given bad data")
+    public void shouldNotUpdateListAndReturnBadRequestWhenWrongId(){
+        //given
+        var createdListResponse = createList(FIRST_LIST_NAME);
+        var listId = createdListResponse.listId();
+        var updateListRequestBody = new UpdateListRequestBody(NEW_LIST_NAME, WRONG_FULL_PRICE);
+        HttpEntity<UpdateListRequestBody> requestEntity = new HttpEntity<>(updateListRequestBody);
+
+        //when
+        var updateListResponse = client.exchange(listPath(listId), PUT, requestEntity, UpdateListRequestBody.class);
+
+        //then
+        assertThat(updateListResponse.getStatusCode(), equalTo(BAD_REQUEST));
+    }
+
+    @Test
+    @DisplayName("Should not update list and return 400 when given bad data")
+    public void shouldNotUpdateListAndReturnBadRequestWhenEmptyNewName(){
+        //given
+        var createdListResponse = createList(FIRST_LIST_NAME);
+        var listId = createdListResponse.listId();
+        var updateListRequestBody = new UpdateListRequestBody(WRONG_LIST_NAME, NEW_FULL_PRICE);
+        HttpEntity<UpdateListRequestBody> requestEntity = new HttpEntity<>(updateListRequestBody);
+
+        //when
+        var updateListResponse = client.exchange(listPath(listId), PUT, requestEntity, UpdateListRequestBody.class);
+
+        //then
+        assertThat(updateListResponse.getStatusCode(), equalTo(BAD_REQUEST));
     }
 }
