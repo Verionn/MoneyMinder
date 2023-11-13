7e843a1b36f8f97da8bdcfda025d84ef0c0a6268
Wikcis <wiktor.ciszewski7734@gmail.com>
2023-11-13 19:10:04 +0100

    refactor item_entity table

--- src/main/resources/db/migration/V1__createTables.sql
+++ src/main/resources/db/migration/V1__createTables.sql
@@ -13,9 +13,8 @@ CREATE TABLE IF NOT EXISTS item_entity (
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL default 0,
     amount INT NOT NULL default 1,
-    category VARCHAR(255),
-    list_id BIGINT,
-    FOREIGN KEY (list_id) REFERENCES list_entity (list_id),
+    category_id BIGINT NOT NULL FOREIGN KEY (category_id) REFERENCES category_entity (category_id),
+    list_id BIGINT FOREIGN KEY (list_id) REFERENCES list_entity (list_id),
     weight BIGINT NOT NULL DEFAULT 0,
     time_created TIMESTAMP WITHOUT TIME ZONE
     );
--- src/main/resources/db/migration/V2__insertData.sql
+++ src/main/resources/db/migration/V2__insertData.sql
@@ -4,65 +4,65 @@ INSERT INTO category_entity (name) VALUES ('Sweets');
 INSERT INTO list_entity (name) VALUES ('Na jutro');
 INSERT INTO list_entity (name) VALUES ('Na weekend');
 
-INSERT INTO item_entity (name, price, amount, category, list_id, weight, time_created)
+INSERT INTO item_entity (name, price, amount, category_id, list_id, weight, time_created)
 VALUES (
         'Bread',
         3.20,
         1,
-        'Food',
+        1,
         1,
         0,
         '2023-11-05 20:13:20'
        );
 
-INSERT INTO item_entity (name, price, amount, category, list_id, weight, time_created)
+INSERT INTO item_entity (name, price, amount, category_id, list_id, weight, time_created)
 VALUES (
         'Milk',
         3.20,
         1,
-        'Food',
+        1,
         1,
         0,
         '2023-11-10 20:13:20'
        );
 
-INSERT INTO item_entity (name, price, amount, category, list_id, weight, time_created)
+INSERT INTO item_entity (name, price, amount, category_id, list_id, weight, time_created)
 VALUES (
         'Potato',
         3.20,
         10,
-        'Food',
+        1,
         1,
         750,
         '2023-08-07 20:13:20'
        );
 
-INSERT INTO item_entity (name, price, amount, category, list_id, weight, time_created)
+INSERT INTO item_entity (name, price, amount, category_id, list_id, weight, time_created)
 VALUES (
         'Carrot',
         3.20,
         3,
-        'Food',
+        1,
         2,
         500,
         '2023-11-11 20:13:20'
        );
 
-INSERT INTO item_entity (name, price, amount, category, list_id, weight, time_created)
+INSERT INTO item_entity (name, price, amount, category_id, list_id, weight, time_created)
 VALUES (
         'Cola',
         3.20,
         1,
-        'Sweets',
+        2,
         2,
         0,
         '2023-09-11 20:13:20'
        );
-INSERT INTO item_entity (name, price, category, list_id, time_created)
+INSERT INTO item_entity (name, price, category_id, list_id, time_created)
 VALUES (
            'Cola',
            3.20,
-           'Sweets',
+           2,
            2,
            '2023-08-11 20:13:20'
        );
