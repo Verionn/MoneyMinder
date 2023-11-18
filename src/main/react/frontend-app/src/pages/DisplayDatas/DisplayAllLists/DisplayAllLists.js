b767e81d8e3d7157b7892f241371b2f8aa5b2a85
Joram Mumb <jorammumb15.jm@gmail.com>
2023-11-19 00:36:21 +0100

    display Items

--- src/main/react/frontend-app/src/pages/DisplayDatas/DisplayAllLists/DisplayAllLists.js
+++ src/main/react/frontend-app/src/pages/DisplayDatas/DisplayAllLists/DisplayAllLists.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GetDatas from "../../../components/communicationWithBackEnd/GetDatas";
 import ListDescription from "../../../components/listDescription/listDescriptions";
 import "./DisplayAllLists.css";
-import GetNumberOfItems from "../../../components/functions/GetDatasFromItems";
+import GetDatasFromItems from "../../../components/functions/GetDatasFromItems";
+import DisplayItems from "../DisplayItems/DisplayItems";
 
 const DisplayAllLists = () => {
+  const [ItemsID, setItemsID] = useState(-1);
+  console.log("ItemsID is : " + ItemsID);
+
+ 
+  const handleListClick = (listId) => {
+    // Do something with the list ID, such as logging or triggering another action
+    console.log(`List clicked with ID: ${listId}`);
+    setItemsID(listId);
+  };
+
+  const handleCloseItemsList = () => {
+    console.log("closeItemsList");
+    setItemsID(-1); 
+  };
+
+
   const apiUrl = "http://localhost:8080/lists";
   let { data, loading, error } = GetDatas({ apiUrl });
-
+  const getListName = (listID) => {
+      if(data && data.lists && data.lists.length > 0) {
+        return data.lists.find(list => list.listId === listID).name;
+      }
+      return "List";
+  }
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -22,16 +44,20 @@ const DisplayAllLists = () => {
       {data && data.lists && data.lists.length > 0 ? (
         <ul>
           {data.lists.map((list) => (
-            <li key={list.listId} className="singleList">
+            <li
+              key={list.listId}
+              className="singleList"
+              onClick={() => handleListClick(list.listId)}
+            >
               <div className="listTitle">{list.name}</div>
               <div className={"listHeader"}>
                 <span className="NumberOfItems">
                   Number of Items :{" "}
-                  <GetNumberOfItems listID={list.listId} operation={"count"} />
+                  <GetDatasFromItems listID={list.listId} operation={"count"} />
                 </span>
                 <span className="FullPrice">
                   Total Price :{" "}
-                  <GetNumberOfItems listID={list.listId} operation={"price"} />{" "}
+                  <GetDatasFromItems listID={list.listId} operation={"price"} />{" "}
                   $
                 </span>
               </div>
@@ -40,6 +66,7 @@ const DisplayAllLists = () => {
                   Description={
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d"
                   }
+                  onClick={() => console.log("Clicked on description")}
                 ></ListDescription>
               </div>
             </li>
@@ -47,6 +74,13 @@ const DisplayAllLists = () => {
         </ul>
       ) : (
         <p>No data available.</p>
+      )}
+      {ItemsID !== -1 && (
+       
+        <div>
+          {/* Content to render when ItemsID is not -1 */}
+          <DisplayItems listID={ItemsID} operation={getListName(ItemsID) }onClose={handleCloseItemsList} />
+        </div>
       )}
     </div>
   );
