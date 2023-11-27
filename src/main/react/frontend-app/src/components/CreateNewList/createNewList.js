400495c187a6cbbba777534fa3052266ab97c898
Joram Mumb <jorammumb15.jm@gmail.com>
2023-11-28 00:25:09 +0100

    create List component

--- src/main/react/frontend-app/src/components/CreateNewList/createNewList.js
+++ src/main/react/frontend-app/src/components/CreateNewList/createNewList.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Button, Modal, Form } from "react-bootstrap";
 import "boxicons";
+import "./createNewList.css";
 
 const CreateNewList = ({ onClose }) => {
   const [show, setShow] = useState(false);
   const [listName, setListName] = useState("");
   const [description, setDescription] = useState("");
   const [descriptionCount, setDescriptionCount] = useState(0);
-  const maxDescriptionLength = 3;
-  const maxListNameLength = 5; // Adjust this according to your requirement
+  const maxDescriptionLength = 300;
+  const maxListNameLength = 50; // Adjust this according to your requirement
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -55,16 +56,22 @@ const CreateNewList = ({ onClose }) => {
 
   return (
     <div>
-      <Button variant="primary" onClick={handleShow}>
-        <box-icon name="plus" border="circle" color="white"></box-icon>
+      <Button
+        variant="primary"
+        onClick={handleShow}
+        active={show}
+        className={"createListButton"}
+      >
+        <box-icon name="plus" color="white"></box-icon>
+        <p>New list</p>
       </Button>
 
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Create a New List</Modal.Title>
+          <Modal.Title>Create a new list</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form.Group controlId="formListName">
+          <Form.Group controlId="formListName" className="formListName">
             <Form.Label>List Name</Form.Label>
             <Form.Control
               type="text"
@@ -73,15 +80,24 @@ const CreateNewList = ({ onClose }) => {
               value={listName}
               onChange={handleInputChange}
             />
-            {(listName.length === 0 || listName.length > maxListNameLength) && (
-              <div className="errorMessage">
-                {listName.length === 0
-                  ? "Please enter a name"
-                  : `Name should be less than ${maxListNameLength} characters`}
+            <div className="listNameHints">
+              <div className="characterCount">
+                {listName.length}/{maxListNameLength}
               </div>
-            )}
+              {(listName.length === 0 ||
+                listName.length > maxListNameLength) && (
+                <div className="errorMessage">
+                  {listName.length > maxListNameLength
+                    ? `Name should be less than ${maxListNameLength} characters`
+                    : ""}
+                </div>
+              )}
+            </div>
           </Form.Group>
-          <Form.Group controlId="formListDescription">
+          <Form.Group
+            controlId="formListDescription"
+            className="formListDescription"
+          >
             <Form.Label>Description</Form.Label>
             <Form.Control
               type="text"
@@ -91,22 +107,34 @@ const CreateNewList = ({ onClose }) => {
               onChange={handleInputChange}
               className="writeDescription"
             />
-            <div className="characterCount">
-              {descriptionCount}/{maxDescriptionLength}
-            </div>
-            {description.length > maxDescriptionLength && (
-              <div className="errorMessage">
-                Description should be less than {maxDescriptionLength} characters.
+            <div className="descriptionHint">
+              <div className="characterCount">
+                {descriptionCount}/{maxDescriptionLength}
               </div>
-            )}
+              {description.length > maxDescriptionLength && (
+                <div className="errorMessage">
+                  Description should be less than {maxDescriptionLength}{" "}
+                  characters.
+                </div>
+              )}
+            </div>
           </Form.Group>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleCreateNewList} disabled={listName.length === 0 || listName.length > maxListNameLength}>
-            Save changes
+          <Button
+            variant="primary"
+            onClick={handleCreateNewList}
+            disabled={
+              listName.length === 0 ||
+              listName.length > maxListNameLength ||
+              descriptionCount > maxDescriptionLength
+            }
+            className="createListButtonConfirm"
+          >
+            Create
           </Button>
         </Modal.Footer>
       </Modal>
