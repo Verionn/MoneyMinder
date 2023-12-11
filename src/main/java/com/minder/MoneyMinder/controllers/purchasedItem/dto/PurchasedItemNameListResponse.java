8b938fb8d8ccf1e9d1b5ffbd593af2a2bf33b657
v3ri0N <bijacik.adam@gmail.com>
2023-12-11 16:45:12 +0100

    add new endpoint to get item names by prefix

---
+++ src/main/java/com/minder/MoneyMinder/controllers/purchasedItem/dto/PurchasedItemNameListResponse.java
@@ -0,0 +1,6 @@
+package com.minder.MoneyMinder.controllers.purchasedItem.dto;
+
+import java.util.List;
+
+public record PurchasedItemNameListResponse(List<PurchasedItemNameResponse> purchasedItemNameResponses) {
+}
