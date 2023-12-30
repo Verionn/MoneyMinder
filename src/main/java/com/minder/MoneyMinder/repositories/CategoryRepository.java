ead89362c4a73e591a887585bf76406d03ebdd6f
v3ri0N <bijacik.adam@gmail.com>
2023-12-30 02:02:57 +0100

    add implementation of users to categories

--- src/main/java/com/minder/MoneyMinder/repositories/CategoryRepository.java
+++ src/main/java/com/minder/MoneyMinder/repositories/CategoryRepository.java
@@ -2,8 +2,15 @@ package com.minder.MoneyMinder.repositories;
 
 import com.minder.MoneyMinder.models.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    List<CategoryEntity> findAllByUserId(Long userId);
+
+    //@Query("SELECT l FROM CategoryEntity l WHERE l.categoryId = :categoryId AND l.userId = :userId")
+    boolean existsByCategoryIdAndUserId(Long categoryId, Long userId);
 }
