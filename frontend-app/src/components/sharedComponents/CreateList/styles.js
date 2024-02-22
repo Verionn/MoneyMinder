6331217b442670057f813f7de629c7d62effd2bc
Joram Mumb <jorammumb15.jm@gmail.com>
2024-02-22 23:11:35 +0100

    update createList

---
+++ frontend-app/src/components/sharedComponents/CreateList/styles.js
@@ -0,0 +1,11 @@
+export const styles = ({ darkMode }) => ({
+  inputBackground: {
+    backgroundColor: darkMode ? "#424242" : "#f5f5f5",
+    color: darkMode ? "white" : "black",
+    fontSize: "1rem",
+
+    letterSpacing: "0.1rem",
+    resize: "none",
+    
+  },
+});
