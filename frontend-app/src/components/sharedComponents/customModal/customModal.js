3464a07c9063075cc2de10cfc12c11c37d3dcd8c
Joram Mumb <jorammumb15.jm@gmail.com>
2024-02-22 17:47:37 +0100

    create List Method

---
+++ frontend-app/src/components/sharedComponents/customModal/customModal.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./customModal.css";
+import { styles } from "./styles";
+import {useContextElements} from "../../../utils/hooks/customHooks";
+export const CustomModal = ({
+  show,
+  onClose,
+  children,
+  ModalTitle,
+  ModalConfirmationButton,
+  functionTOCall,
+}) => {
+    
+    const {isDarkMode} = useContextElements();
+  if (!show) {
+    return null;
+  }
+  const style = styles({ darkMode: isDarkMode });
+  return (
+    <div className="custom-modal-backdrop">
+      <div className="custom-modal">
+        <div className="custom-modal-header" style={{...style.header}}>
+          <h2>{ModalTitle}</h2>
+          <button onClick={onClose} className="custom-modal-close-button" style={{...style.closeButton}}>
+            &times;
+          </button>
+        </div>
+        <div className="custom-modal-body" style={{...style.body}}>{children}</div>
+        <div className="custom-modal-footer"  style={{...style.footer}}>
+          <button onClick={onClose} className="custom-modal-footer-button">
+            Close
+          </button>
+          <button
+            onClick={functionTOCall}
+            style={{ backgroundColor: "var(--primary-color)" }}
+            className="custom-modal-footer-button"
+          >
+            {ModalConfirmationButton}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomModal;
