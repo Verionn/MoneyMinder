aa8b098505488455027882922fa7f74923c765d1
Joram Mumb <jorammumb15.jm@gmail.com>
2024-03-04 16:05:24 +0100

    protected Routes

--- frontend-app/src/components/Body/Body.js
+++ frontend-app/src/components/Body/Body.js
@@ -4,12 +4,11 @@ import { appInfo, loginBtn, Credit } from "../../utils/datas/appInfo";
 import { useContextElements } from "../../utils/hooks/customHooks";
 import { Styles } from "./styles";
 import Section from "../sections/section";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import RenderRoutes from "../../utils/router/Routes";
 import { LoginPage } from "../../utils/datas/appInfo";
 const Body = () => {
   const { isDarkMode, handleResize, windowWidth } = useContextElements();
-  const [isLogin] = useState(false);
 
   useEffect(() => {
     window.addEventListener("resize", handleResize);
@@ -22,13 +21,16 @@ const Body = () => {
 
   return (
     <div className="appStates" style={{ ...styles.root }}>
-      {isLogin ? (
+      {!localStorage.getItem("token") ? (
         <div className="appBody">
           <SideBar appInfo={appInfo} login={loginBtn} Credit={Credit} />
           <Section />
         </div>
       ) : (
-        <RenderRoutes routes={LoginPage} style={{ ...styles.root }} />
+        <>
+          <RenderRoutes routes={LoginPage} style={{ ...styles.root }} />
+          
+        </>
       )}
     </div>
   );
