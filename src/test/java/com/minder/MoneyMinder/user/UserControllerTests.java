52a515d94345e0acd451a7fac127f6415ea11c8e
v3ri0N <bijacik.adam@gmail.com>
2024-03-05 16:37:35 +0100

    add tests

--- src/test/java/com/minder/MoneyMinder/user/UserControllerTests.java
+++ src/test/java/com/minder/MoneyMinder/user/UserControllerTests.java
@@ -2,6 +2,7 @@ package com.minder.MoneyMinder.user;
 
 import com.minder.MoneyMinder.MoneyMinderApplicationTests;
 import com.minder.MoneyMinder.controllers.category.dto.CategoryResponse;
+import com.minder.MoneyMinder.controllers.user.dto.ChangePasswordRequest;
 import com.minder.MoneyMinder.controllers.user.dto.LoginRequest;
 import com.minder.MoneyMinder.controllers.user.dto.LoginResponse;
 import com.minder.MoneyMinder.controllers.user.dto.RegisterUserRequest;
@@ -138,5 +139,71 @@ public class UserControllerTests extends MoneyMinderApplicationTests {
         //then
         assertThat(loginResponse.getStatusCode(), is(equalTo(UNAUTHORIZED)));
         assertThat(loginResponse.getBody(), is(nullValue()));
+    }
+
+    @Test
+    @DisplayName("Should return 200 and change password")
+    public void should200AndChangePassword(){
+        runAsUser();
+        var loginResponse = client.exchange(prepareUrl(CHANGE_PASSWORD_PATH), HttpMethod.POST,
+                new HttpEntity<>(new ChangePasswordRequest(VALID_USER_PASSWORD, VALID_USER_PASSWORD, VALID_NEW_USER_PASSWORD)), LoginResponse.class);
+
+        //then
+        assertThat(loginResponse.getStatusCode(), is(equalTo(OK)));
+    }
+
+    @Test
+    @DisplayName("Should return 400 when given wrong new password")
+    public void should400WhenGivenWrongNewPassword(){
+        runAsUser();
+        var loginResponse = client.exchange(prepareUrl(CHANGE_PASSWORD_PATH), HttpMethod.POST,
+                new HttpEntity<>(new ChangePasswordRequest(VALID_USER_PASSWORD, VALID_USER_PASSWORD, INVALID_USER_PASSWORD)), LoginResponse.class);
+
+        //then
+        assertThat(loginResponse.getStatusCode(), is(equalTo(BAD_REQUEST)));
+    }
+
+    @Test
+    @DisplayName("Should return 400 when given not the same old passwords")
+    public void should400WhenGivenNotTheSameOldPasswords(){
+        runAsUser();
+        var loginResponse = client.exchange(prepareUrl(CHANGE_PASSWORD_PATH), HttpMethod.POST,
+                new HttpEntity<>(new ChangePasswordRequest(VALID_USER_PASSWORD, INVALID_USER_PASSWORD, VALID_USER_PASSWORD)), LoginResponse.class);
+
+        //then
+        assertThat(loginResponse.getStatusCode(), is(equalTo(BAD_REQUEST)));
+    }
+
+    @Test
+    @DisplayName("Should return 400 when given wrong old passwords")
+    public void should400WhenGivenWrongPassword(){
+        runAsUser();
+        var loginResponse = client.exchange(prepareUrl(CHANGE_PASSWORD_PATH), HttpMethod.POST,
+                new HttpEntity<>(new ChangePasswordRequest(INVALID_USER_PASSWORD, VALID_USER_PASSWORD, INVALID_USER_PASSWORD)), LoginResponse.class);
+
+        //then
+        assertThat(loginResponse.getStatusCode(), is(equalTo(BAD_REQUEST)));
+    }
+
+    @Test
+    @DisplayName("Should return 403 when trying to change password without token")
+    public void should403WhenTryingToChangePasswordWithoutToken(){
+        runWithoutToken();
+        var loginResponse = client.exchange(prepareUrl(CHANGE_PASSWORD_PATH), HttpMethod.POST,
+                new HttpEntity<>(new ChangePasswordRequest(VALID_USER_PASSWORD, VALID_USER_PASSWORD, VALID_USER_PASSWORD)), LoginResponse.class);
+
+        //then
+        assertThat(loginResponse.getStatusCode(), is(equalTo(FORBIDDEN)));
+    }
+
+    @Test
+    @DisplayName("Should return 400 when new password is the same as old")
+    public void should400WhenNewPasswordIsSameAsOld(){
+        runAsUser();
+        var loginResponse = client.exchange(prepareUrl(CHANGE_PASSWORD_PATH), HttpMethod.POST,
+                new HttpEntity<>(new ChangePasswordRequest(VALID_USER_PASSWORD, VALID_USER_PASSWORD, VALID_USER_PASSWORD)), LoginResponse.class);
+
+        //then
+        assertThat(loginResponse.getStatusCode(), is(equalTo(BAD_REQUEST)));
     }
 }
