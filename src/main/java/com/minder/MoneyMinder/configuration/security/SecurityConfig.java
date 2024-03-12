946389a1d3fe6edb686e69efaa422d9c81bdaf48
v3ri0N <bijacik.adam@gmail.com>
2024-03-12 15:22:51 +0100

    add tests, change post to put, fix docs

--- src/main/java/com/minder/MoneyMinder/configuration/security/SecurityConfig.java
+++ src/main/java/com/minder/MoneyMinder/configuration/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.minder.MoneyMinder.models.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.AbstractRequestMatcherRegistry;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(new AntPathRequestMatcher("/users/login"))
                         .permitAll()
