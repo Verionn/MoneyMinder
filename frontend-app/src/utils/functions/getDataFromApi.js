a321b6cb0de3f187a63e779c7d8bba1c0f7bbe9c
Joram Mumb <jorammumb15.jm@gmail.com>
2024-03-16 00:43:45 +0100

    removing gen folder

--- frontend-app/src/utils/functions/getDataFromApi.js
+++ frontend-app/src/utils/functions/getDataFromApi.js
@@ -8,7 +8,6 @@ export const GetDataFromApi = ({ apiUrl, options = {} }) => {
 
   const fetchOptions = useMemo(() => {
     const token = localStorage.getItem("token");
-
     const headers = {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
